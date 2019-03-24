25b512f99ececeb82f5a833e3b7e50a55c210ad7
jackey <jackey_hong@163.com>
2019-03-24 13:01:13 +0800

    account.py 示例

---
+++ account.py
@@ -0,0 +1,3 @@
+ak = 'your AccessKeyID'  # 示例： ak ='dLicnRJnlkjdfEjklLjkklf'
+sc = 'your AccessKeySecret'   # sc = 'KljliMlkjiLjijiljHljk'
+SubDomain = 'your SubDomain'    # SubDomain = 'sub.domain.com'
